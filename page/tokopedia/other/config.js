c2b76b0688aa1ff92db4ff80494d6bf9665c8523
Patrama <40189879+Patrama@users.noreply.github.com>
2026-07-13 15:58:56 +0700

    Update config.js

--- page/tokopedia/other/config.js
+++ page/tokopedia/other/config.js
@@ -2,4 +2,4 @@
 // when swapping in a different Google Sheet.
 
 // 🔗 PASTE YOUR PUBLISHED GOOGLE SHEET CSV LINK HERE
-const SPREADSHEET_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/YOUR_ID_HERE/pub?output=csv';
+const SPREADSHEET_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRMSVSL-88iVwW3uH0JQdTYIVWUkwMOs-_xfVh1c2RGY5qhAFDlP_SJlY2nw8P3c5dDimDYuMku6DGR/pub?gid=979915917&single=true&output=csv';
