6358760a7d627bbd5a6f4b27b9ff90b6cc7a6b00
Patrama <40189879+Patrama@users.noreply.github.com>
2026-07-13 15:16:03 +0700

    Adding Tokopedia Apreadsheet Page

---
+++ page/tokopedia/img/README.txt
@@ -0,0 +1,5 @@
+Not currently used by index.html — this table-style layout has no
+product images, just Number / Category / Name / Link.
+
+If you later add thumbnails, drop image files here and reference
+them from page/index.html as ../img/filename.jpg
---
+++ page/tokopedia/other/config.js
@@ -0,0 +1,5 @@
+// Site configuration — this is the only file you should need to touch
+// when swapping in a different Google Sheet.
+
+// 🔗 PASTE YOUR PUBLISHED GOOGLE SHEET CSV LINK HERE
+const SPREADSHEET_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/YOUR_ID_HERE/pub?output=csv';
